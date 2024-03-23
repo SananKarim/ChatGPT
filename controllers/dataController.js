ead5b6d499635eb01fda9cfcd3e86538b085e8b3
SananKarim <65340880+SananKarim@users.noreply.github.com>
2024-03-23 17:41:32 +0500

    mongo files

---
+++ controllers/dataController.js
@@ -0,0 +1,22 @@
+const db = require("../db/db.js"); 
+module.exports.getAllUsers = async (req, res) => {
+  try {
+    if (!db.readyState) {
+      return res.status(500).json({ success: false, error: "Database connection failed" });
+    }
+
+    const usersCollection = db.collection('user'); 
+
+    const users = await usersCollection.find().toArray();
+
+    return res.status(200).json({ success: true, data: users });
+  } catch (error) {
+    console.error("Error fetching user data from database:", error);
+
+    return res.status(500).json({
+      success: false,
+      error: "Internal server error",
+      details: error.message, 
+    });
+  }
+};
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -4,5 +4,6 @@ const authController = require("../controllers/authController");
 const router = express.Router();
 
 router.post("/login", authController.login_post);
+router.post("/data", authController.data);
 
 module.exports = router;
