57a84836b811bdeaa128fd9fe757520982fd990f
SananKarim <65340880+SananKarim@users.noreply.github.com>
2024-03-23 16:08:21 +0500

    changes in index

--- index.js
+++ index.js
@@ -1,23 +1,24 @@
 const express = require("express");
-const jwt = require("jsonwebtoken");
 const authenticateToken = require("./middleware/auth");
-const authRoutes = require("./routes/authRoutes");
-body_parser = require("body-parser");
-
+const bodyParser = require("body-parser");
 
 const app = express();
+
 app.use(express.static("public"));
-app.use(express.json());
-app = express().use(body_parser.json());
+app.use(bodyParser.json());
 
-
-app.get("/healthpoint",  (req, res) => {
+app.get("/healthpoint", (req, res) => {
   res.status(200).json("connected");
+});
+
+app.get("/protected", authenticateToken, (req, res) => {
+  res.status(200).send("Access granted");
 });
 
 app.listen(3001 || 1337, () => {
   console.log("Server started");
 });
+
 // app.get("/protected", authenticateToken, (req, res) => {
 //   res.status(200).send("Access granted");
 // });
