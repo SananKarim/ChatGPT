be712c9e3017688820a5e49aced36232bfa558a5
SananKarim <65340880+SananKarim@users.noreply.github.com>
2024-03-23 17:28:41 +0500

    mongo link vercel

--- db/db.js
+++ db/db.js
@@ -1,14 +1,13 @@
 const mongoose = require("mongoose");
 
 const MONGODB_URI =
-  "mongodb+srv://vercel-admin-user-65fec38b7dd1410a8e3f8952:4GDm3Ptk4THu1QJA@cluster0.t9his3p.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
-
+  "mongodb+srv://vercel-admin-user-65fec3f408f2a92ee20a382b:ZluLczPX9fCeh2Uy@cluster0.t9his3p.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 mongoose
   .connect(MONGODB_URI)
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => {
     console.error("Error connecting to MongoDB:", err);
-    process.exit(1); // Exit the application if database connection fails
+    process.exit(1); 
   });
 
 module.exports = mongoose.connection;
